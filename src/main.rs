d8f3a7f66c91988c0bd11c8ea3a8d82bc1db4ba9
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2023-06-22 07:46:31 +0900

    ui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use eframe::egui;
-use egui::{emath::RectTransform, FontData, FontDefinitions, Rounding};
+use egui::{emath::RectTransform, FontData, FontDefinitions, Rounding, ScrollArea};
 use std::env;
 
 fn main() -> Result<(), eframe::Error> {
@@ -11,19 +11,25 @@ fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(800.0, 600.0)),
         centered: true,
-        default_theme: eframe::Theme::Light,
+        default_theme: eframe::Theme::Dark,
         follow_system_theme: false,
         ..Default::default()
     };
-    eframe::run_native("UDP Tester", options, Box::new(|cc| Box::new(MyApp::new(cc))))
+    eframe::run_native(
+        "UDP Tester",
+        options,
+        Box::new(|cc| Box::new(MyApp::new(cc))),
+    )
 }
 
-struct MyApp {}
+struct MyApp {
+    send_input: String,
+}
 
 impl MyApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self {
-            //default value
+            send_input: String::new(), //default value
         }
     }
 }
@@ -32,10 +38,11 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // ctx.request_repaint(); //毎フレーム更新を要求
 
+        /*
         let mut fonts = FontDefinitions::default();
         fonts.font_data.insert(
             "my_font".to_owned(),
-            FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\yumin.ttf")),
+            FontData::from_static(include_bytes!("C:\\Windows\\Fonts\\yumindb.ttf")),
         );
         fonts
             .families
@@ -48,10 +55,38 @@ impl eframe::App for MyApp {
             .unwrap()
             .push("my_font".to_owned());
         ctx.set_fonts(fonts);
+        */
 
         // ---
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello world!");
+            ui.heading("UDP Tester");
+            ui.horizontal(|ui| {
+                ui.label("Send:");
+
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+                    if ui.button("SEND").clicked() {
+                        println!("{}", self.send_input);
+                    }
+                    //ui.visuals_mut().extreme_bg_color = egui::Color32::RED;
+                    ui.add_sized(
+                        ui.available_size(),
+                        egui::TextEdit::singleline(&mut self.send_input),
+                    );
+                });
+            });
+
+            ui.add_space(4.0);
+
+            let row_height = ui.text_style_height(&egui::TextStyle::Body);
+            let rows = 100;
+            ScrollArea::vertical()
+                .stick_to_bottom(true)
+                .auto_shrink([false; 2])
+                .show_rows(ui, row_height, rows, |ui, row_range| {
+                    for row in row_range {
+                        ui.label(format!("{:?}", row));
+                    }
+                });
         });
     }
 }
